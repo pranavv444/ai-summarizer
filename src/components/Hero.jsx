65d220ec738ae50e2537147437cc05b574590ec9
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-09-26 02:59:27 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+      <img src={logo} alt='ai_logo' className='w-28 object-contain'/>
+      <button type='button' onClick={()=>window.open('https://github.com/pranavv444')} className='black_btn'>GitHub</button>
+      </nav>
+      <h1 className='head_text'>Summarize Articles <br className='max-md:hidden'/>
+      <span className='orange_gradient'>Open AI GPT-4</span></h1>
+      <h2 className='desc'>Simplify your articles readings!!</h2>
+    </header>
+  )
+}
+
+export default Hero
